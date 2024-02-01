edb21e433999984984b8d48969009df372a26912
Malte Franken <exxamalte@users.noreply.github.com>
2024-02-01 21:47:12 +1100

    Bump haversine to 2.8.1 (#21)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ URL = "https://github.com/exxamalte/python-aio-quakeml-client"
 
 REQUIRES = [
     "aiohttp>=3.7.4,<4",
-    "haversine>=2.7.0",
+    "haversine>=2.8.1",
     "xmltodict>=0.13.0",
     "dateparser>=1.1.6",
 ]
