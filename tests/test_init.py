29c13ee7dccde8fd60c7800cdb685f747e8902fe
Malte Franken <exxamalte@users.noreply.github.com>
2026-06-20 20:30:49 +1000

    fully migrate to pyproject

--- tests/test_init.py
+++
@@ -1,8 +0,0 @@
-"""Tests for QuakeML feed general setup."""
-
-from aio_quakeml_client import __version__
-
-
-def test_version():
-    """Test for version tag."""
-    assert __version__ is not None
